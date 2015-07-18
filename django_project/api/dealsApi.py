c59801d7fb5ec3849e87332ec416314e24cb431c
Ashmeet Singh <hackercomapny@gmail.com>
2015-07-18 14:12:02 +0530

    scafolding

--- django_project/api/dealsApi.py
+++ django_project/api/dealsApi.py
@@ -86,6 +86,7 @@ def get_deals(request,user, category, typ):
             search.update({"address.text":{"$in":[re.compile(x.replace("_"," "),re.IGNORECASE) for x in request.GET['area'].split(",")]}})
         if 'name' in request.GET.keys():
         	search.update({"vendor_name":re.compile(request.GET['name'],re.IGNORECASE)})
+        return HttpResponse(search)
         if 'type' in request.GET.keys():
             search.update({"type":{"$in":[re.compile(x.replace("_"," "),re.IGNORECASE) for x in request.GET['type'].split(",")]}})
         if 'rate' in request.GET.keys():
