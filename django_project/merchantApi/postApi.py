87b21686df6071bca272a21317205e8a6be192ad
bassoGeorge <anishgeorgehb@gmail.com>
2015-07-20 16:41:36 +0530

    Small update to merchant app

--- django_project/merchantApi/postApi.py
+++ django_project/merchantApi/postApi.py
@@ -80,7 +80,9 @@ def login(request):
             cred = collection.find_one({"username": data['username'], "password": data['password']})
             if cred:
                 vendor = db.merchants.find_one({"vendor_id": cred['vendor_id']}, {"vendor_name": True, "_id": False})
-                result = {"result": True, "vendor_name": vendor['vendor_name'], "vendor_id": cred['vendor_id']}
+                result = {"result": True, data: {
+                              "vendor_name": vendor['vendor_name'], "vendor_id": cred['vendor_id']
+                            }}
                 if 'address' in vendor:
                     result.update({"address": vendor['address']})
                 return response(result)
