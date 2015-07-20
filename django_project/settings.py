9fafa76976f90a98f05ab84c9640af41ff500654
Ashmeet <hackercompany@gmail.com>
2015-07-20 14:17:25 +0530

    blah

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
 #    'corsheaders.middleware.CorsMiddleware',
-    'dcors.dcorsmiddleware.CorsMiddleware',
+#   'dcors.dcorsmiddleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
