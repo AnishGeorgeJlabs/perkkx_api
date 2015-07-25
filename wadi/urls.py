37a255d0cdae95ac4659f291e84ce689232933ec
bassoGeorge <anishgeorgehb@gmail.com>
2015-07-25 09:16:31 +0530

    wadi stuff

--- wadi/urls.py
+++ wadi/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, patterns
 from . import api
+from httpproxy.views import HttpProxy
 
 urlpatterns = patterns(
     '',
     url(r'^$', api.test, name='test'),
-    url(r'^block', api.block_number, name='block_number')
+    url(r'^block', api.block_number, name='block_number'),
+    url(r'^hproxy/(P<url>.*)$', HttpProxy.as_view(base_url='http://hymnary.org/'))
 )
