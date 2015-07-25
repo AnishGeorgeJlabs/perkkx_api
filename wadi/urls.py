1d8c9bc6f5843d7df1d200b833755616a7324e1b
bassoGeorge <anishgeorgehb@gmail.com>
2015-07-25 09:21:35 +0530

    wadi stuff

--- wadi/urls.py
+++ wadi/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', api.test, name='test'),
     url(r'^block', api.block_number, name='block_number'),
-    url(r'^hproxy/(P<url>.*)$', HttpProxy.as_view(base_url='http://hymnary.org/'))
+    url(r'^hproxy/(?P<url>.*)$', HttpProxy.as_view(base_url='http://hymnary.org/'))
 )
