f5e919ed14a88db88ec65fc451de5c4a3594bd38
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-12 17:28:34 +0530

    upload file issue

--- wadi/utility/uApi.py
+++ wadi/utility/uApi.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 def reference(request):
     filename = request.GET['file']
@@ -9,6 +10,8 @@ def reference(request):
     }
     return render(request, 'wadi/file.html',context)
 
+@csrf_exempt
 def upload(request):
     file = request.FILES['upload']
     name = request.POST['file']
+    return HttpResponse(json.dumps({"success": name}))
