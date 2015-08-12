9a98c24efe2537d5aec18bccafa543e1914c2538
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-12 17:19:11 +0530

    upload file issue

--- wadi/urls.py
+++ wadi/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns(
     url(r'^block', api.block_number, name='block_number'),
     url(r'^hproxy/(?P<url>.*)$', HttpProxy.as_view(base_url='http://hymnary.org/')),
     url(r'^utility', uApi.reference, name='utility'),
+    url(r'^upload', uApi.upload, name='upload'),
 
     url(r'^interface/login$', interfaceApi.login, name='interface.login'),
     url(r'^interface/post$', interfaceApi.formPost, name='interface.post'),
