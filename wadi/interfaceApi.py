d284a7d65c0934a87b22ecb91540297e8ca1f16c
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-17 15:30:35 +0530

    Added support for channel operation

--- wadi/interfaceApi.py
+++ wadi/interfaceApi.py
@@ -8,6 +8,7 @@ from external.sheet import get_scheduler_sheet
 import calendar
 
 monthDict = dict((v, k) for k,v in enumerate(calendar.month_name))
+lasttouch_dict = (db.form.find_one({"operation": "channel"}, {"regex": True}))['regex']
 
 @csrf_exempt
 def login(request):
@@ -58,6 +59,7 @@ def query(request):
     obj = db.queries.find_one({"_id": ObjectId(id)})
     if obj:
         options = obj['target_config']
+        # Customisation ----------------- #
         if 'customer' not in options:
             options['mode'] = 'all'
         else:
@@ -70,6 +72,9 @@ def query(request):
         if 'purchase_month' in options:
             options['purchase_month'] = [monthDict[a] for a in options['purchase_month']]
 
+        if 'channel' in options:
+            options['channel'] = map(lambda k: lasttouch_dict[k], options['channel'])
+        # ------------------------------- #
         pipeline = [k for k, v in options.items() if k != 'mode']
         pipeline.append('customer')
 
@@ -78,7 +83,7 @@ def query(request):
         raise Http404
 
 def get_form_data(request):
-    data = db.form.find({}, {"_id": False})
+    data = db.form.find({}, {"_id": False, "regex": False})
     '''
     result = {}
     for item in data:
