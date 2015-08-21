6fe9194f13a4f910c36124847dcb2b430192d58b
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-21 13:14:50 +0530

    Added cat in search

--- django_project/api/search.py
+++ django_project/api/search.py
@@ -1,7 +1,7 @@
 from bson.json_util import dumps
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.template import Template,Context
+from django.template import Template, Context
 import pymongo
 import datetime
 import random
@@ -11,17 +11,23 @@ import math
 import re
 from unidecode import unidecode
 
-failure = dumps({ "success": 0 })
+failure = dumps({"success": 0})
 dbclient = pymongo.MongoClient("mongodb://45.55.232.5:27017")
 db = dbclient.perkkx
 
+
 @csrf_exempt
 def search(request):
-	merchants = db.merchants
-	try:
-		q = request.GET['q']
-	except:
-		return HttpResponse(failure)
-	result = merchants.distinct("vendor_name",{"vendor_name":re.compile(q,re.IGNORECASE)})
-	success = dumps({"success" : 1, "data":result})
-	return HttpResponse(success, content_type="application/json")
+    merchants = db.merchants
+    try:
+        q = request.GET['q']
+    except:
+        return HttpResponse(failure)
+    query = {"vendor_name": re.compile(q, re.IGNORECASE)}
+    if 'cat' in request.GET:
+        cats = map(lambda k: int(k), request.GET['cat'].split(','))
+        query.update({'cat': {"$in": cats}})
+
+    result = merchants.distinct("vendor_name", query)
+    success = dumps({"success": 1, "data": result})
+    return HttpResponse(success, content_type="application/json")
