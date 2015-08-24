631b67091dd4d05ed16e9033b640fbc1cecc2616
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-24 17:02:04 +0530

    Secured Mongodb

--- django_project/api/__init__.py
+++ django_project/api/__init__.py
@@ -1,4 +1,5 @@
 import pymongo
 
-dbclient = pymongo.MongoClient("mongodb://perkkxAdmin:perkkx%40123@45.55.232.5:27017")
+dbclient = pymongo.MongoClient("45.55.232.5:27017")
+dbclient.perkkx.authenticate('perkkxAdmin', 'perkkx@123', mechanism='MONGODB-CR')
 db = dbclient.perkkx
