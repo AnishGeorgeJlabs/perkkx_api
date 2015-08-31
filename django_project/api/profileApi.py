c9feaf7af3b12d3680da5fbf395063743dfd2614
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-31 15:03:06 +0530

    debugging

--- django_project/api/profileApi.py
+++ django_project/api/profileApi.py
@@ -79,7 +79,7 @@ def pre_app_check(request):
             if ocount == 1:
                 pending = db.order_data.find_one({"userID": userID, "ustatus": "pending"})
                 if pending:
-                    data['rcode'] = json.dumps(pending)
+                    data['rcode'] = pending['rcode']
                     merchant = db.merchants.find_one({'vendor_id': pending['vendor_id']}, {"_id": False, "vendor_name": True})
                     if not merchant:
                         data['vendor_name'] = "_"
